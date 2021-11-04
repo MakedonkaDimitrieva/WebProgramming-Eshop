8c6b89cc45ada7fdce84b26ce1097a55e4e15b04
MakedonkaDimitrieva <makedonka.dimitrieva@students.finki.ukim.mk>
2021-11-04 21:31:49 +0100

    session management

--- src/main/java/mk/ukim/finki/wpaud/bootstrap/DataHolder.java
+++ src/main/java/mk/ukim/finki/wpaud/bootstrap/DataHolder.java
@@ -1,6 +1,7 @@
 package mk.ukim.finki.wpaud.bootstrap;
 
 import mk.ukim.finki.wpaud.model.Category;
+import mk.ukim.finki.wpaud.model.User;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -11,10 +12,14 @@ import java.util.List;
 public class DataHolder {
 
     public static List<Category> categories = new ArrayList<>();
+    public static List<User> users = new ArrayList<>();
 
     @PostConstruct // da se povika pri startuvanje na app
     public void init() {
         categories.add(new Category("Books", "Books category"));
         categories.add(new Category("Movies", "Movies category"));
+
+        users.add(new User("kostadin.mishev", "km", "Kostadin", "Mishev"));
+        users.add(new User("riste.stojanov", "rs", "Riste", "Stojanov"));
     }
 }
