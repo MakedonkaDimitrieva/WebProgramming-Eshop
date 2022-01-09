c0d06d14288b67a1db12e0b34b86cd1d82042405
MakedonkaDimitrieva <makedonka.dimitrieva@students.finki.ukim.mk>
2022-01-09 23:41:09 +0100

    spring security with custom authentication

--- src/main/java/mk/ukim/finki/wpaud/web/controller/RegisterController.java
+++ src/main/java/mk/ukim/finki/wpaud/web/controller/RegisterController.java
@@ -1,8 +1,9 @@
 package mk.ukim.finki.wpaud.web.controller;
 
+import mk.ukim.finki.wpaud.model.enumerations.Role;
 import mk.ukim.finki.wpaud.model.exceptions.InvalidArgumentsException;
 import mk.ukim.finki.wpaud.model.exceptions.PasswordsDoNotMatchException;
-import mk.ukim.finki.wpaud.service.AuthService;
+import mk.ukim.finki.wpaud.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/register")
 public class RegisterController {
 
-    private final AuthService authService;
+    private final UserService userService;
 
-    public RegisterController(AuthService authService) {
-        this.authService = authService;
+    public RegisterController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
@@ -35,9 +36,10 @@ public class RegisterController {
                            @RequestParam String password,
                            @RequestParam String repeatedPassword,
                            @RequestParam String name,
-                           @RequestParam String surname) {
+                           @RequestParam String surname,
+                           @RequestParam Role role) {
         try {
-            this.authService.register(username, password, repeatedPassword, name, surname);
+            this.userService.register(username, password, repeatedPassword, name, surname, role);
             return "redirect:/login";
         } catch (PasswordsDoNotMatchException | InvalidArgumentsException exception) {
             return "redirect:/register?error=" + exception.getMessage();
