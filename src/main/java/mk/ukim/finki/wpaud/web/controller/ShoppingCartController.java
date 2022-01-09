2fa4c2c765a6ddfc1e1e3abced7055ec00672301
MakedonkaDimitrieva <makedonka.dimitrieva@students.finki.ukim.mk>
2022-01-09 04:47:53 +0100

    spring security in-memory authentication

--- src/main/java/mk/ukim/finki/wpaud/web/controller/ShoppingCartController.java
+++ src/main/java/mk/ukim/finki/wpaud/web/controller/ShoppingCartController.java
@@ -1,7 +1,6 @@
 package mk.ukim.finki.wpaud.web.controller;
 
 import mk.ukim.finki.wpaud.model.ShoppingCart;
-import mk.ukim.finki.wpaud.model.User;
 import mk.ukim.finki.wpaud.service.ShoppingCartService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,8 +24,8 @@ public class ShoppingCartController {
             model.addAttribute("hasError", true);
             model.addAttribute("error", error);
         }
-        User user = (User) req.getSession().getAttribute("user");
-        ShoppingCart shoppingCart = this.shoppingCartService.getActiveShoppingCart(user.getUsername());
+        String username = req.getRemoteUser();
+        ShoppingCart shoppingCart = this.shoppingCartService.getActiveShoppingCart(username);
         model.addAttribute("products", this.shoppingCartService.listAllProductsInShoppingCart(shoppingCart.getId()));
         model.addAttribute("bodyContent", "shopping-cart");
         return "master-template";
@@ -35,8 +34,8 @@ public class ShoppingCartController {
     @PostMapping("/add-product/{id}")
     public String addProductToShoppingCart(@PathVariable Long id, HttpServletRequest req) {
         try {
-            User user = (User) req.getSession().getAttribute("user");
-            ShoppingCart shoppingCart = this.shoppingCartService.addProductToShoppingCart(user.getUsername(), id);
+            String username = req.getRemoteUser();
+            this.shoppingCartService.addProductToShoppingCart(username, id);
             return "redirect:/shopping-cart";
         } catch (RuntimeException exception) {
             return "redirect:/shopping-cart?error=" + exception.getMessage();
